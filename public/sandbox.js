275ff2ed696c0a4a5e3f3f2c36e4d4202ef900d9
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-02-01 17:21:05 +0100

    20230201

---
+++ public/sandbox.js
@@ -0,0 +1,15 @@
+"use strict";
+/* let greet = Function; */
+let greet;
+greet = (name, greating) => {
+    console.log(`${name} says ${greating}`);
+};
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
