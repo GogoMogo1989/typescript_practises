e89299571b7c85cf02b0a477e4296387a6f85dba
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-02-02 13:17:41 +0100

    20230202

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,4 @@
-class Invoice {
-    constructor(
-        public client: string,
-        private details: string,
-        readonly amount: number,
-    ){}
-
-    format(){
-        return`${this.client} owes Ł${this.amount} for ${this.details}`
-    }
-}
+import {Invoice} from './classes/invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250)
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
