a8dd82dfca652fe88b69c411c4e2ba87f9dc3630
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-02-02 13:27:11 +0100

    20230202

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: "shaun",
+    age: 30,
+    speak(text: string): void{
+        console.log(text)
+    },
+    spend(amount: number): number{
+        console.log('i spent', amount);
+        return amount
+    }
+} 
+
+const greatPerson = (person: IsPerson) => {
+    console.log('hello', person.name)
+}
+ 
+greatPerson(me)
+
+
 import {Invoice} from './classes/invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250)
