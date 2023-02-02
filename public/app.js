0ebaacd5f56856fef16c5f1b384e2b8a324602b9
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-02-02 13:27:59 +0100

    20230202

--- public/app.js
+++ public/app.js
@@ -1,18 +1,3 @@
-const me = {
-    name: "shaun",
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('i spent', amount);
-        return amount;
-    }
-};
-const greatPerson = (person) => {
-    console.log('hello', person.name);
-};
-greatPerson(me);
 import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,3 @@
-interface IsPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
-}
-
-const me: IsPerson = {
-    name: "shaun",
-    age: 30,
-    speak(text: string): void{
-        console.log(text)
-    },
-    spend(amount: number): number{
-        console.log('i spent', amount);
-        return amount
-    }
-} 
-
-const greatPerson = (person: IsPerson) => {
-    console.log('hello', person.name)
-}
- 
-greatPerson(me)
-
 
 import {Invoice} from './classes/invoice.js';
 
