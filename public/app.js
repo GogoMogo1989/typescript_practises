c98a6a09326f0c57b51e6687b7d35c4e44c672df
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-02-02 12:38:27 +0100

    20230202

--- public/app.js
+++ public/app.js
@@ -1,15 +1,5 @@
 "use strict";
-/* const anchor = document.querySelector('a')!
-
-/* if(anchor){
-    console.log(anchor.href)
-} */
-/* console.log(anchor.href)
-
- */
-/* const form = document.querySelector('form')!; */
 const form = document.querySelector('.new-item-form');
-/* console.log(form.children) */
 const type = document.querySelector('#type');
 const toform = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,8 @@
-/* const anchor = document.querySelector('a')!
 
-/* if(anchor){
-    console.log(anchor.href)
-} */
-/* console.log(anchor.href)
 
- */ 
-
-/* const form = document.querySelector('form')!; */
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-/* console.log(form.children) */
+
 
 const type = document.querySelector('#type')! as HTMLSelectElement;
 const toform = document.querySelector('#tofrom')! as HTMLInputElement;
